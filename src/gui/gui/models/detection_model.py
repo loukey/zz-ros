3946493473d053f484ae546ba309bd1f48beef3d
loukey <neu.ycl.za@gmail.com>
2025-07-04 13:40:22 +0800

    fix bug

--- src/gui/gui/models/detection_model.py
+++ src/gui/gui/models/detection_model.py
@@ -24,6 +24,7 @@ class DetectionModel(QObject):
         self.detection_enabled = False
         self.yolo_segmentor = YOLOSegmentor("./detection_models/yolo_multi_seg_n.pt")
         self.latest_detection_result = self.yolo_segmentor.detect(cv2.imread("./test3.jpg"))
+        self.latest_detection_result['depth'] = 100
         self.detect_timer = QTimer()
         self.detect_timer.setInterval(1000)
         self.detect_timer.timeout.connect(self.process_detection)
