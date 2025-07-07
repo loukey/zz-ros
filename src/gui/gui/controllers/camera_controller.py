0519e25ca69a54adb216b0bdcbca27037cfc883e
loukey <neu.ycl.za@gmail.com>
2025-07-07 11:29:23 +0800

    fix move2part bug

--- src/gui/gui/controllers/camera_controller.py
+++ src/gui/gui/controllers/camera_controller.py
@@ -6,6 +6,7 @@ from .base_controller import BaseController
 import numpy as np
 import cv2
 from kinematic import HandEyeTransform
+from math import pi
 
 
 class CameraController(BaseController):
@@ -312,6 +313,8 @@ class CameraController(BaseController):
                     depth = detections['depth']
                     angle = detections['angle']
                     theta_list = self.hand_eye_transform.get_theta_list(central_center, angle)
+                    origin_theta_list = [0, -pi/2, 0, pi/2, 0, 0]
+                    theta_list = (np.array(theta_list) - np.array(origin_theta_list)).tolist()
                     self.display(f"中心点: {central_center}, 深度: {depth}, 角度: {angle}, 角度列表: {theta_list}", "控制")
                     self.send_angles_requested.emit({
                         'target_angles': theta_list,
